bce6c40a1000f8e695a41e8ad21ee67bbaa33091
Ambroarun83 <arunnatarajan2704@gmail.com>
2024-07-24 12:49:52 +0530

    Git Change

---
+++ api/config-file.php
@@ -0,0 +1,4 @@
+<?php
+$HOSTPATH = "https://" . $_SERVER['HTTP_HOST'] . "/template/";
+
+define('HOSTPATH', $HOSTPATH);
---
+++ include/views/company_creation.php
@@ -0,0 +1,39 @@
+<div class="row gutters">
+    <div class="col-12">
+        <div class="card wow">
+            <div class="card-header">
+                <div class="card-title">Test Card</div>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-12">
+                        <table class="table custom-table dtable">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Class</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
